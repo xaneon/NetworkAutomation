fdcb846a6d0c1ee4a904f43ff29f330201257ef3
Bonne <bonne.habekost@gmail.com>
2019-07-01 16:07:56 +0200

    final cleanup

--- tenant_cleanup.py
+++
@@ -1,9 +0,0 @@
-from Kap5_SDN_odl_vtn import *
-import json
-
-tens = json.loads(list_tenants().text)
-for i in tens['vtns']['vtn']:
-    remove_tenant(i['name'])
-r = list_tenants()
-remove_tenant('vtn1')
-print(r.text)
