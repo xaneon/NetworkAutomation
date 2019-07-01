e95e42fe802fa07a70b23d998bf19d7c7a9def9b
Bonne <bonne.habekost@gmail.com>
2019-07-01 15:56:57 +0200

    renamed a few files

--- history_20190212.py
+++
@@ -1,5 +0,0 @@
-# coding: utf-8
-get_ipython().run_cell_magic('timeit', '-r 10', 'for elem in range(0, 1000):\n  elem*elem\n  \n')
-get_ipython().run_cell_magic('timeit', '-r 10', '[elem*elem for elem in range(0, 1000)]\n\n')
-import numpy as np
-get_ipython().run_cell_magic('timeit', '-r 10', 'np.arange(0, 1000) * np.arange(0, 1000)\n\n')
